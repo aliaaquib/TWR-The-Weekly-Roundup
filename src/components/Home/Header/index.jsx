31b87d14397592f6d6bab68c5846f20c61b020ab
imaaquibali <imaaquibali@gmail.com>
2023-06-17 01:11:41 +0530

    TWR - The Weekly Roundup

---
+++ src/components/Home/Header/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css'
+
+const Header = () => (
+  <header className='home-header'>
+    <h2>Welcome to</h2>
+    <h1>
+      <span>“</span> TWR - The Weeky Roundup <span>”</span>
+    </h1>
+    <p>
+      An weekly newsletter <br /> focused on web3, open-source, life lesions.
+    </p>
+  </header>
+);
+
+export default Header;
---
+++ src/config/data.js
@@ -0,0 +1,76 @@
+export const blogList = [
+    {
+      id: 1,
+      title: 'Unveiling the World of NFTs: A Comprehensive Guide ',
+      category: 'NFT',
+      subCategory: ['web3', 'art', 'nft'],
+      description:
+      "Exploring the world of NFTs: from their definition and technology to their impact on industries and notable examples. Discover the revolution of unique digital assets",
+      authorName: 'Aaquib Ali',
+      authorAvatar: '/assets/images/author.jpg',
+      createdAt: 'June 10, 2023',
+      cover: '/assets/images/blog1.png',
+    },
+    {
+      id: 2,
+      title: 'Coding Efficiency Unleashed: 10 ChatGPT Hacks for Developers',
+      category: 'AI',
+      subCategory: ['chatgpt', 'ai', 'developer'],
+      description:
+        "Unleash ChatGPT's power to optimize your coding workflow. Discover 10 game-changing commands that will save time, troubleshoot, generate code snippets, and enhance your productivity as a developer.",
+      authorName: 'Aaquib Ali',
+      authorAvatar: '/assets/images/author.jpg',
+      createdAt: 'June 03, 2023',
+      cover: '/assets/images/blog2.png',
+    },
+    {
+      id: 3,
+      title: 'ChatGPT vs Bard: Which chatbot is better?',
+      category: 'AI',
+      subCategory: ['chatgpt', 'ai', 'brad'],
+      description:
+        "A comparison of AI chatbots in terms of instruction, understanding, reasoning, creativity, and speed. Discover their strengths and weaknesses in this insightful article.",
+      authorName: 'Aaquib Ali',
+      authorAvatar: '/assets/images/author.jpg',
+      createdAt: 'May 27, 2023',
+      cover: '/assets/images/blog3.png',
+    },
+    {
+      id: 4,
+      title: 'Top 5 in Demand Programming Languages to learn in 2023',
+      category: 'Programming',
+      subCategory: ['developer', 'programming', 'language'],
+      description:
+        "Discover the top 5 in-demand programming languages for 2022. From JavaScript to Solidity, explore their learning duration, job opportunities, and features in this insightful article.",
+      authorName: 'Aaquib Ali',
+      authorAvatar: '/assets/images/author.jpg',
+      createdAt: 'May 20, 2023',
+      cover: '/assets/images/blog4.png',
+    },
+    {
+      id: 5,
+      title: 'A complete Beginner’s guide — Web 3.0',
+      category: 'Web3',
+      subCategory: ['guide', 'begainner', 'programming'],
+      description:
+        "Explore the evolution of the web from Web 1.0 to Web 3.0 in this informative article. Discover the features, differences, and unique characteristics of each phase, including the potential of Web 3.0 to revolutionize our online experiences.",
+      authorName: 'Aaquib Ali',
+      authorAvatar: '/assets/images/author.jpg',
+      createdAt: 'May 13, 2023',
+      cover: '/assets/images/blog5.png',
+    },
+    {
+      id: 6,
+      title: 'Web3: An Introduction to the Decentralized Future of the Internet',
+      category: 'Web3',
+      subCategory: ['language', 'decentralized', 'programming'],
+      description:
+        "Discover the decentralized world of web3 and how it revolutionizes online interactions. Explore blockchain, cryptocurrencies, and smart contracts in this informative article and join The VerSiton community for support and resources.",
+      authorName: 'Aaquib Ali',
+      authorAvatar: '/assets/images/author.jpg',
+      createdAt: 'May 06, 2023',
+      cover:
+        '/assets/images/blog6.png',
+    },
+  ];
+  
